ff9414d4ee17181f16e5d0966ba1231646020485
blakeaj <MSOEnumber2@gmail.com>
2020-05-09 17:35:01 -0500

    I made the code for the sorting algorithm based off of the pseudocode in the wikipedia page.

--- sorting.py
+++ sorting.py
@@ -1,2 +1,23 @@
 
-def 
+# theArray is just a given array of values.
+def cocktail_shaker_sort(theArray):
+    swapped = true
+    while swapped:
+        swapped = false
+        # Do swapping in one direction.
+        for i in range(0, len(theArray) - 1, 1):
+            if theArray[i] > theArray[i + 1]:
+                temp = theArray[i]
+                theArray[i] = theArray[i + 1]
+                theArray[i + 1] = temp
+                swapped = true
+        if swapped == false:
+            break
+        swapped = false
+        # Do swapping in the other direction.
+        for i in range(len(theArray) - 1, 0, -1):
+            if theArray[i] > theArray[i + 1]:
+                temp = theArray[i]
+                theArray[i] = theArray[i + 1]
+                theArray[i + 1] = temp
+                swapped = true
