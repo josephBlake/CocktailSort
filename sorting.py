398b17376d5071b52bb4c680bdfdfab3f84227b0
blakeaj <MSOEnumber2@gmail.com>
2020-05-09 17:49:39 -0500

    Fixed boolean values.

--- sorting.py
+++ sorting.py
@@ -1,23 +1,23 @@
 
 # theArray is just a given array of values.
 def cocktail_shaker_sort(theArray):
-    swapped = true
+    swapped = True
     while swapped:
-        swapped = false
+        swapped = False
         # Do swapping in one direction.
         for i in range(0, len(theArray) - 1, 1):
             if theArray[i] > theArray[i + 1]:
                 temp = theArray[i]
                 theArray[i] = theArray[i + 1]
                 theArray[i + 1] = temp
-                swapped = true
-        if swapped == false:
+                swapped = True
+        if swapped == False:
             break
-        swapped = false
+        swapped = False
         # Do swapping in the other direction.
         for i in range(len(theArray) - 1, 0, -1):
             if theArray[i] > theArray[i + 1]:
                 temp = theArray[i]
                 theArray[i] = theArray[i + 1]
                 theArray[i + 1] = temp
-                swapped = true
+                swapped = True
