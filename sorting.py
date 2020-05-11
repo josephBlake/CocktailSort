ce686d5da63af5a5cbdf5593192bcd00ef80e39b
lindsleyae1 <lindsleyae@msoe.edu>
2020-05-11 00:55:51 -0500

    Wrote some simple tests and found an issue with the current algorithm

--- sorting.py
+++ sorting.py
@@ -1,23 +1,23 @@
-
-# theArray is just a given array of values.
-def cocktail_shaker_sort(theArray):
-    swapped = True
-    while swapped:
-        swapped = False
-        # Do swapping in one direction.
-        for i in range(0, len(theArray) - 1, 1):
-            if theArray[i] > theArray[i + 1]:
-                temp = theArray[i]
-                theArray[i] = theArray[i + 1]
-                theArray[i + 1] = temp
-                swapped = True
-        if swapped == False:
-            break
-        swapped = False
-        # Do swapping in the other direction.
-        for i in range(len(theArray) - 1, 0, -1):
-            if theArray[i] > theArray[i + 1]:
-                temp = theArray[i]
-                theArray[i] = theArray[i + 1]
-                theArray[i + 1] = temp
-                swapped = True
+class CocktailShaker:
+    # theArray is just a given array of values.
+    def cocktail_shaker_sort(theArray):
+        swapped = True
+        while swapped:
+            swapped = False
+            # Do swapping in one direction.
+            for i in range(0, len(theArray) - 1, 1):
+                if theArray[i] > theArray[i + 1]:
+                    temp = theArray[i]
+                    theArray[i] = theArray[i + 1]
+                    theArray[i + 1] = temp
+                    swapped = True
+            if swapped == False:
+                break
+            swapped = False
+            # Do swapping in the other direction.
+            for i in range(len(theArray) - 1, 0, -1):
+                if theArray[i] > theArray[i + 1]:
+                    temp = theArray[i]
+                    theArray[i] = theArray[i + 1]
+                    theArray[i + 1] = temp
+                    swapped = True
---
+++ test_cocktail.py
@@ -0,0 +1,36 @@
+import unittest
+import random
+from sorting import CocktailShaker
+
+class CocktailShakerTests(unittest.TestCase):
+    def test_sorted_case(self):
+        array = [random.random() for x in range(100)]
+        array.sort()
+        sorted = array[:]
+        CocktailShaker.cocktail_shaker_sort(array)
+        print(array)
+        print(sorted)
+        self.assertEqual(len(sorted), len(array))
+
+    def test_reverse_sorted_case(self):
+        array = [random.random() for x in range(100)]
+        array.sort(reverse=True)
+        sorted = array[:]
+        sorted.sort()
+        CocktailShaker.cocktail_shaker_sort(array)
+        print(array)
+        print(sorted)
+        self.assertEqual(len(sorted), len(array))
+
+    def test_random_case(self):
+        array = [random.random() for x in range(100)]
+        random.shuffle(array)
+        sorted = array[:]
+        sorted.sort()
+        CocktailShaker.cocktail_shaker_sort(array)
+        print(array)
+        print(sorted)
+        self.assertEqual(len(sorted), len(array))
+
+if __name__ == "__main__":
+    unittest.main()
